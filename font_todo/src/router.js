41868dc2613dd575241f837a8b95cb7648363026
eventime <alextonywang@gmail.com>
2023-05-30 23:20:55 +0800

    import vuex, router

---
+++ font_todo/src/router.js
@@ -0,0 +1,8 @@
+const router = new router({
+    routes: [
+      { path: '/', component: Home },
+      { path: '/about', component: About },
+      // 其他路由配置
+    ]
+  });
+  
---
+++ font_todo/src/store.js
@@ -0,0 +1,23 @@
+// store.js
+
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    // 定义你的状态
+  },
+  mutations: {
+    // 定义修改状态的方法
+  },
+  actions: {
+    // 定义异步操作的方法
+  },
+  getters: {
+    // 定义获取状态的方法
+  }
+})
+
+export default store
